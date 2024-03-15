7d0b087908835700fff532804890bf38d958494d
Lehyoumi <163418646+Lehyoumi@users.noreply.github.com>
2024-03-15 10:06:48 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+
+function mudarimagemfora()
+{
+  document.getElementById("imagem").src="https://static-cdn.jtvnw.net/jtv_user_pictures/80853f2b-4148-4079-aa1f-ef997e059c3d-profile_banner-480.png";
+}
+
+function mudarimagemdentro()
+{
+  document. getElementById("imagem").src="https://pbs.twimg.com/media/FqpiAu9WAAACFV4.png";
+}
